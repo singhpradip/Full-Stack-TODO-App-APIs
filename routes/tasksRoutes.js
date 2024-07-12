1f5eb009c16b6e7d2865cbe1021b5e18c28c123b
pradiptechie <kumarpradip3956@gmail.com>
2024-07-12 16:04:42 +0545

    update on task routes

--- routes/tasksRoutes.js
+++ routes/tasksRoutes.js
@@ -11,7 +11,7 @@ const router = express.Router();
 router.use(verifyAccessToken);
 
 router.post("/create-task", createTask);
-router.get("get-tasks/", getTasks);
+router.get("/get-tasks", getTasks);
 router.put("update-task/:id", updateTask);
 router.delete("delete-task/:id", deleteTask);
 
